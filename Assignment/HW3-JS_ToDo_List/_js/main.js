f1284b1b67c3bc37f14e5080088183988e311cec
koalaboy808 <carlo_liquido@ischool.berkeley.edu>
2016-09-14 23:17:13 -0700

    adding HW3 material

---
+++ Assignment/HW3-JS_ToDo_List/_js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(
+    $("#new-item").on('click', function() {
+        // once the document loads, create new item with this function
+    })
+);
+
+$("#list_todo").on('click', "button", function() {
+        // move from list_todo container to list_completed container
+});
+
+$("#list_completed").on('click', "button", function() {
+        // move back from list_completed container to list_todo container
+});
