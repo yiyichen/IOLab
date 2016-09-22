ef72c84cd4aeff22b5e0d6ba54773026ff96bac0
koalaboy808 <carlo_liquido@ischool.berkeley.edu>
2016-09-22 13:27:39 -0700

    upload skeleton without title spaces

---
+++ Assignment/HW4_Soundcloud/startercode_HW4_skeleton.js
@@ -0,0 +1,27 @@
+// Event hander for calling the SoundCloud API using the user's search query
+function callAPI(query) {
+	$.get("https://api.soundcloud.com/tracks?client_id=b3179c0738764e846066975c2571aebb",
+		{'q': query,
+		'limit': '200'},
+		function(data) {
+			// PUT IN YOUR CODE HERE TO PROCESS THE SOUNDCLOUD API'S RESPONSE OBJECT
+			// HINT: CREATE A SEPARATE FUNCTION AND CALL IT HERE
+		},'json'
+	);
+}
+
+// 'Play' button event handler - play the track in the Stratus player
+function changeTrack(url) {
+	// Remove any existing instances of the Stratus player
+	$('#stratus').remove();
+
+	// Create a new Stratus player using the clicked song's permalink URL
+	$.stratus({
+      key: "b3179c0738764e846066975c2571aebb",
+      auto_play: true,
+      align: "bottom",
+      links: url
+    });
+}
+
+
