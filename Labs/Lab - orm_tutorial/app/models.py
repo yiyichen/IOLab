ae21e51dfef913aae5f0d38d5d8dbd3205cedd5f
yiyi <yiyichen928@gmail.com>
2016-10-12 12:59:54 -0700

    testing out orm

--- Labs/Lab - orm_tutorial/app/models.py
+++ Labs/Lab - orm_tutorial/app/models.py
@@ -1,3 +1,10 @@
 from app import db
 
 # Your Customer Database code should go here
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    company = db.Column(db.String(120), unique = False)
+    email = db.Column(db.String(120))
+
+    def __repr__(self):
+        return '<customer %r>' % self.id
--- Labs/Lab - orm_tutorial/app/views.py
+++ Labs/Lab - orm_tutorial/app/views.py
@@ -10,6 +10,13 @@ def index():
 @app.route('/create_customer', methods=['GET', 'POST'])
 def create_customer():
     form = CustomerForm()
+    if form.validate_on_submit():
+        customer = models.Customer(
+        company = form.company.data,
+        email = form.email.data)
+
+        db.session.add(customer)
+        db.session.commit()
     #Capture form data and send to database
         return redirect('/customers')
     return render_template('customer.html', form=form)
@@ -17,5 +24,6 @@ def create_customer():
 @app.route('/customers')
 def display_customer():
     #Fetch customer data from database
+    customers = models.Customer.query.all()
     return render_template('home.html',
                             customers=customers)
